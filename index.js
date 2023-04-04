d45e893a85a16d1d34ea3c3da230b5f8f401cf55
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-04 10:58:45 +0300

    rendered in HTML

--- index.js
+++ index.js
@@ -2,4 +2,25 @@
 const url = "http://localhost:3000/Cars"
 fetch(url)
 .then(res=> res.json())
-.then(data => data.forEach(car => renderCars(car)))
+.then(data => data.forEach(car =>renderCars(car)))
+//rendering cars
+    function renderCars(car){
+        const cars=document.createElement("span")
+        cars.className = "card1"
+        cars.innerHTML=`
+        <img src=${car.image} height="200px">
+        <P>${car.name}<br>
+        Brand:${car.brand}<br>
+        Price:${car.price}<br>
+        Condition:${car.condition}<br>
+        Mileage:${car.mileage}<br>
+        Top speed:${car.topspeed}<br>
+        Fuel consumption:${car.fuelconsumption}<br>
+        <button>Buy</button>
+        </p>
+
+        `
+
+        document.getElementById("card").appendChild(cars)
+    }
+
