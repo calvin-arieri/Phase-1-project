43d5b578d24b075df4f543c773212810d9fd23c7
calvin-arieri <126255829+calvin-arieri@users.noreply.github.com>
2023-04-10 23:48:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ addEventListener('DOMContentLoaded',fetchingFunction())
 
 // fetching from db.json
 function fetchingFunction(){
-const url = "db.json"
+const url = "http://localhost:3000/Cars"
 fetch(url)
 .then(res=> res.json())
 .then(data => data.forEach(car =>renderCars(car)))
